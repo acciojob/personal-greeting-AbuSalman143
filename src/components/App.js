4ab5f7fa3e9dc5bde2933d9c8dc63fb5f4a717e4
Abu Salman <121804012+AbuSalman143@users.noreply.github.com>
2024-05-24 20:57:49 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
-import React from "react";
+import React ,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  
+  let [greet,setGreet]=useState("");
+  let [input,setInput]=useState("");
+
+  function upadteInput(e){
+    let value=e.target.value;
+    setInput(value)
+    setGreet("Hello "+value+" !")
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <input 
+          type="text"
+          value={input}
+          onChange={upadteInput}
+        />
+      <p>{greet}</p> 
     </div>
   )
 }
